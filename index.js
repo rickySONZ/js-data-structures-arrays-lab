d497335c57203f033efda1edfcc60daaef177cd0
Ryan Erricson <ryanerricson@gmail.com>
2021-05-03 13:45:32 -0400

    commit tree ff20c18fe32c58d679947ab1b6d0184fea7f59a9

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name)
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name)
+}
+
+function appendDriver(name) {
+    const newMenu = [...drivers.slice(0, drivers.length), name] 
+    return newMenu
+}
+
+function prependDriver(name) {
+    const newMenu = [name, ...drivers.slice(0, drivers.length)] 
+    return newMenu
+}
+
+function removeLastDriver(name) {
+    const newMenu = drivers.slice(0, drivers.length - 1)
+    return newMenu
+}
+
+function removeFirstDriver(name) {
+    const newMenu = drivers.slice(1, drivers.length)
+    return newMenu
+}
